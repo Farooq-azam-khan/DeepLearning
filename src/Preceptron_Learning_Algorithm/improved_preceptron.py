ba46b749b6960239865c162db804c129830d80af
Farooq Azam Khan <farooq1.khan@ryerson.ca>
2018-05-13 00:16:07 -0400

    need to improve some func on IP.py

--- src/Preceptron_Learning_Algorithm/improved_preceptron.py
+++ src/Preceptron_Learning_Algorithm/improved_preceptron.py
@@ -1,7 +1,9 @@
 import random # used to generate random weights
 import math
+from vector import Vector
 
-# TODO: debug overflow and algorithm
+# TODO: debug errors
+
 # activation function
 def sign(num):
     if (num >= 0):
@@ -10,7 +12,6 @@ def sign(num):
         return -1
 
 def sigmoid(x):
-    x = round(x, 2)
     return 1 / (1 + math.exp(-x))
 
 def dsigmoid(y):
@@ -28,18 +29,21 @@ class Improved_Preceptron():
         #learning rate
         self.lr = 0.1
 
-        # preceptron keeps track of its own weights
-        self.input_weights = []
-        self.output_weights = []
+        #len will be number of inputs
+        self.input_weights = Vector(self.num_weights)
+        # len will be number of outputs
+        self.output_weights = Vector(self.num_outputs)
 
-        self.input_bias = []
-        self.output_bias = []
+        self.input_weights.randomize()
+        self.output_weights.randomize()
+        self.input_bias = Vector(1)    # len will be 1
+        self.output_bias = Vector(self.num_outputs)    # len will be number of biases
 
-        self.randomize_input_weights()
-        self.randomize_output_weigths()
-        self.randomize_input_bias()
-        self.randomize_output_bias()
+        self.input_bias.randomize()
+        self.output_bias.randomize()
 
+    def randmtd(self):
+        return self.input_weights
 
 
 
@@ -47,238 +51,87 @@ class Improved_Preceptron():
         param: inputs array
         return: expected output
     '''
-    def predict(self, inputs):
-        return self.feed_forward(inputs)
-
-    def feed_forward(self, inputs):
-        preceptron_summation = 0
-        # loop through the weights
-        for i, weight in enumerate(self.input_weights):
-            # multiply the weights by the inputs at that index
-            result = weight * inputs[i]
-            # add it to total sum
-            preceptron_summation = preceptron_summation + result
+    def feed_forward(self, input_array):
+        # feed_forward to preceptron
+        inputs = Vector.to_vector(input_array)
+        preceptron_summation = Vector.dot(inputs, self.input_weights)
         # add the bias
-        preceptron_summation += self.input_bias[0] # array of 1 value
+        preceptron_summation += self.input_bias.data[0] # array of 1 value
         # pass the sum through the activation function
-        preceptron_summation = round(preceptron_summation, 30)
         preceptron_summation = sigmoid(preceptron_summation)
 
-        # more than one output
-        output = []
-        for indx, o_weight in enumerate(self.output_weights):
-            # multiply the weights and add the bias (bias is array of n elements)
-            get_output = (preceptron_summation*o_weight)+self.output_bias[indx]
-            # pass through activation function
-            get_output = round(get_output, 30)
-            output.append(sigmoid(get_output))
-
-        return output
+        # feed_forward to output
+        output = Vector(self.num_outputs)
+        output = Vector.scalar_mul(self.output_weights, preceptron_summation)
+        output = output + self.output_bias
+        output.map(sigmoid)
+        return output.to_array()
 
     '''
     param input: data you want to use to train preceptron
     param target: the known output for adjusting the weights ie the label
     '''
-    def train(self, inputs, targets):
-        preceptron_summation = 0
-        # loop through the weights
-        for indx, weight in enumerate(self.input_weights):
-            preceptron_summation += weight * inputs[indx]
+    def train(self, input_array, target_array):
 
+        inputs = Vector.to_vector(input_array)
+        targets = Vector.to_vector(target_array)
+
+        # feed_forward to preceptron
+        preceptron_summation = Vector.dot(inputs, self.input_weights)
         # add the bias
-        preceptron_summation += self.input_bias[0]
+        preceptron_summation += self.input_bias.data[0] # array of 1 value
         # pass the sum through the activation function
-        preceptron_summation = round(preceptron_summation, 30)
         preceptron_summation = sigmoid(preceptron_summation)
 
-        guesses = []
-        for indx, o_weight in enumerate(self.output_weights):
-            # multiply the weights and add the bias (bias is array of n elements)
-            get_output = (preceptron_summation*o_weight)+self.output_bias[indx]
-            get_output = round(get_output, 30)
-            activation = sigmoid(get_output)
-            # pass through activation function
-            guesses.append(activation)
+        # feed_forward to output
+        outputs = Vector(self.num_outputs)
+        outputs = Vector.scalar_mul(self.output_weights, preceptron_summation)
+        outputs = outputs + self.output_bias
+        outputs.map(sigmoid)
 
-        # get the error = known answer - guess
-        output_errors = []
-        for target, guess in zip(targets, guesses):
-            output_errors.append(target - guess)
+        # --------- update output weights and biases ---------
+        # output error
+        output_errors = targets - outputs
+        output_gradient = Vector.static_map(outputs, dsigmoid)
+        delta_weight_output = output_errors * Vector.scalar_mul(output_gradient,preceptron_summation)
+        delta_weight_output = Vector.scalar_mul(delta_weight_output, self.lr)
 
-        # adjust the output weights here
-        for indx, o_weight in enumerate(self.output_weights):
-            output_gradient = dsigmoid(o_weight)
-            output_error = output_errors[indx]
-            delta_o_weight = self.lr * output_error * output_gradient * preceptron_summation
-            self.output_weights[indx] = o_weight + delta_o_weight
-            self.output_bias[indx] += output_gradient
+        self.output_weights = self.output_weights + delta_weight_output
+        self.output_bias = self.output_bias + output_gradient
 
-        # ------ Adjust the input weights and biases here ------
-        # find the error
-        input_errors = [] # will only be one error because only one node
-        input_error_sum = 0
-        for indx, output_error in enumerate(output_errors):
-            input_error_sum += self.output_weights[indx]
-        input_errors.append(input_error_sum)
-        # find the gradient
-        input_gradient = self.lr * input_errors[0]
+        # --------- update output weights and biases ---------
+        # input error
+        input_errors = Vector.dot(self.output_weights, output_errors)
+        input_gradient = Vector.static_map(inputs, dsigmoid)
+        delta_weight_input =  input_errors * Vector.dot(inputs, input_gradient)
+        delta_weight_input = delta_weight_input * self.lr
 
-        for indx, input_weight in enumerate(self.input_weights):
-            delta_i_weights = self.lr * input_errors[0] * input_gradient * inputs[indx]
-            self.input_weights[indx] += delta_i_weights
+        # adust weights and biases for input
 
-        # adjsut the input biases
-        self.input_bias[0] += input_gradient
+        self.input_weights.scalar_add(delta_weight_input)
+        self.input_bias.scalar_add(delta_weight_input)
 
-
-    '''
-        runs train function over and over agian
-    '''
-    def fit(self, inputs_train_array, targets_train_array, inputs_test_array, targets_test_array):
-        EPOCHS = 10
-        BATCHSIZE = 100
-        for epoch in range(EPOCHS):
-            for _ in range(BATCHSIZE):
-                random_index = random.randrange(len(inputs_train_array))
-                input = inputs_train_array[random_index]
-                target = targets_train_array[random_index]
-                self.train(input, target)
-            acc = self.actual_accuracy(inputs_test_array, targets_test_array)
-            print("Epoch: {} out of {} accuarcy: {:.2f}".format(epoch+1, EPOCHS, acc))
-
-
-            if acc == 1.0:
-                break
-
-    def actual_accuracy(self, inputs_test_array, targets_test_array):
-        average = 0
-        # loop through the points
-        for x, y in zip(inputs_test_array, targets_test_array):
-            # get the inputs
-            prediction = self.feed_forward(x)
-            if prediction == y:
-                average+=1
-
-        return average / len(inputs_test_array)
-
-    def train_test_split(self, X, y, train_split=0.5):
-        # this shuffles data keeping the mapping of the two lists in check
-        '''
-        src: https://stackoverflow.com/questions/13343347/randomizing-two-lists-and-maintaining-order-in-python
-        a = ["Spears", "Adele", "NDubz", "Nicole", "Cristina"]
-        b = [1, 2, 3, 4, 5]
-        combined = list(zip(a, b))
-        random.shuffle(combined)
-        a[:], b[:] = zip(*combined)
-        '''
-
-        combined = list(zip(X, y))
-        random.shuffle(combined)
-        X[:], y[:] = zip(*combined)
-
-
-        splitting_index_X = int(len(X)*train_split)
-        splitting_index_y = int(len(y)*train_split)
-
-        X_train = X[:splitting_index_X]
-        y_train = y[:splitting_index_y]
-        X_test  = X[splitting_index_X:]
-        y_test  = y[splitting_index_y:]
-        return X_train, y_train, X_test, y_test
-
-    def randomize_input_weights(self):
-        for weight_index in range(self.num_weights):
-            # initalize the weights to be random number between -1 and 1
-            self.input_weights.append(random.uniform(-1,1))
-    def randomize_output_weigths(self):
-        for weight_index in range(self.num_outputs):
-            # initalize the weights to be random number between -1 and 1
-            self.output_weights.append(random.uniform(-1,1))
-    def randomize_input_bias(self):
-        for _ in range(1): # only one bias for the inputs
-            self.input_bias.append(random.uniform(-1,1))
-    def randomize_output_bias(self):
-        for _ in range(self.num_outputs):
-            self.output_bias.append(random.uniform(-1,1))
-
-    '''
-        param: array of points objects
-        return: accuracy between 0 and 1
-    '''
-    def accuracy(self, points):
-
-        average = 0
-        # loop through the points
-        for point in points:
-            # get the inputs
-            inputs = point.get_points()
-            prediction = self.feed_forward(inputs)
-            if prediction == point.label:
-                average+=1
-
-        return average / len(points)
-
-    '''
-        param: point for which you want to know the y value
-        usage: get the y value of the linear function
-    '''
-    def guess_y(self, x):
-        # w0*x + w1*y + w2*b
-        w0 = self.weights[0]
-        w1 = self.weights[1]
-        w2 = self.weights[2]
-
-        m = -(w0/w1)
-        b = -(w2/w1) # * 1
-        y = m * x + b
-        return y
-
-    '''
-        param: point (x,y) for which you want to know the z value
-        usage: get the z value of the plane
-    '''
-    def guess_z(self, x, y):
-        # w0x + w1y + w2z + w3b = 0
-        if len(self.weights) == 4:
-            w0 = self.weights[0]
-            w1 = self.weights[1]
-            w2 = self.weights[2]
-            w3 = self.weights[3]
-
-            m1 = -(w0/w2)
-            m2 = -(w1/w2)
-            b = -(w3/w2) # *1
-            z = m1*x + m2*y + b
-            return z
+    def __repr__(self):
+        ret1 = "(input)  weights: {}, bias: {} \n".format(self.input_weights, self.input_bias)
+        ret2 = "(output) weights: {}, biases: {}".format(self.output_weights, self.output_bias)
+        return ret1+ret2
 
 
 def main():
-    or_preceptron = Improved_Preceptron(2, 1)
-    #     input 1, input 2, bias
-    val0 = [0,0]
-    val1 = [0,1]
-    val2 = [1,0]
-    val3 = [1,1]
-    val_labels = [[0], [1], [1], [1]]
-    inputs = [val0, val1, val2, val3]
-
+    p = Improved_Preceptron(2, 1)
+    inputs = [[0,0], [0,1], [1,0], [1,1]]
+    labels = [[0], [1], [1], [1]]
+    p.feed_forward([1, 1])
     for _ in range(100):
-        rand_num = random.randrange(0, 4)
-        input = inputs[rand_num]
-        label = val_labels[rand_num]
-        or_preceptron.train(input, label)
+        rand_indx = random.randrange(len(inputs))
+        input = inputs[rand_indx]
+        label = labels[rand_indx]
+        p.train(input, label)
 
-    print("x or y")
-    for i in inputs:
-        # return -1 or 1
-        pred = or_preceptron.feed_forward(i)
-        # print("pred", pred)
-        if pred == -1:
-            pred = 0
-        else:
-            pred = 1
-        print("{} | {} -> {}".format(i[0], i[1], pred))
-
+    print("0 0", p.feed_forward([0,0]))
+    print("0 1", p.feed_forward([0,1]))
+    print("1 0", p.feed_forward([1,0]))
+    print("1 1", p.feed_forward([1,1]))
 
 if __name__ =="__main__":
     main()
--- src/Preceptron_Learning_Algorithm/vector.py
+++ src/Preceptron_Learning_Algorithm/vector.py
@@ -18,7 +18,40 @@ class Vector():
         for indx, _ in enumerate(self.data):
             self.data[indx] = random.uniform(-1, 1)
 
-    @staticmethod # could use __mult__
+    @staticmethod
+    def scalar_mul(vec, num):
+        ret = Vector(0)
+        for val in vec.data:
+            ret.append(val * num)
+        ret.dim = len(vec.data)
+        return ret
+
+    def append(self, val):
+        self.data.append(val)
+    def set_dim(self, dim):
+        self.dim = dim
+
+    @staticmethod
+    def to_vector(arr):
+        ret = Vector(0)
+        for val in arr:
+            ret.append(val)
+        ret.set_dim(len(arr))
+        return ret
+
+        return ret
+
+    def __mul__(self, other):
+        if (self.dim == other.dim):
+            ret = Vector(0)
+            for val_self, val_other in zip(self.data, other.data):
+                ret.append(val_self * val_other)
+            ret.dim = self.dim
+            return ret
+        else:
+            print("must be same dim")
+            return None
+    @staticmethod
     def dot(vec1, vec2):
         if vec1.dim == vec2.dim:
             sum = 0
@@ -26,8 +59,27 @@ class Vector():
                 sum += (val1 * val2)
             return sum
         else:
-            print("need to be same dimension")
+            print("need to be same dimension", vec1.dim, vec2.dim)
             return None
+
+    def to_array(self):
+        return self.data
+
+    def map(self, func):
+        for indx, val in enumerate(self.data):
+            self.data[indx] = func(val)
+
+    @staticmethod
+    def static_map(vec, func):
+        ret = Vector(0)
+        for val in vec.data:
+            ret.append(func(val))
+        ret.dim = len(vec.data)
+        return ret
+
+    def scalar_add(self, num):
+        for indx, val in enumerate(self.data):
+            self.data[indx] += num
 
     def __add__(self, other):
         if self.dim == other.dim:
